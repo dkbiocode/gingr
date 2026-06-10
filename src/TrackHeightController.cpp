25ed1cf8771879e0749bd23eb3f2121408a64502
meekrob <davidcking.inbox@gmail.com>
2026-06-10 09:27:45 -0600

    paring down src dependency on harvest tools

--- src/TrackHeightController.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright © 2014, Battelle National Biodefense Institute (BNBI);
-// all rights reserved. Authored by: Brian Ondov, Todd Treangen, and
-// Adam Phillippy
-//
-// See the LICENSE.txt file included with this software for license information.
-
-#include "TrackHeightController.h"
